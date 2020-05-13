99cf0f8a268ebf55212324913c1651a5353e2160
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-13 22:07:18 +0000

    fix linter errors

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
-import { Book, removeBook, addBookToLibrary } from './logic.js';
-
+/* eslint-disable no-undef, func-names */
 let myLibrary = JSON.parse(window.localStorage.getItem('myLibrary'));
 
-if(!myLibrary){
-myLibrary = [
-  new Book(2, 'Game Of Thrones', 'A nice book', 100, false, 'https://m.media-amazon.com/images/I/51GlEGbfXQL._SY346_.jpg'),
-  new Book(1, 'The Witcher', 'A nice book', 150, false, 'https://m.media-amazon.com/images/I/51GlEGbfXQL._SY346_.jpg'),
-];
-window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+if (!myLibrary) {
+  myLibrary = [
+    new Book(2, 'Game Of Thrones', 'A nice book', 'George Martin', 100, false, 'https://m.media-amazon.com/images/I/51GlEGbfXQL._SY346_.jpg'),
+    new Book(1, 'The Witcher', 'A nice book', 'The witcher', 150, false, 'https://m.media-amazon.com/images/I/51GlEGbfXQL._SY346_.jpg'),
+  ];
+  window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
 const book = ({
-  id, title, description, numberOfPages, read, image
+  id, title, description, numberOfPages, read, image, author,
 }) => `
   <div id='book-${id}' class="col mb-4">
     <div class="card h-100">
@@ -19,10 +18,15 @@ const book = ({
       <div class="card-body">
         <h5 class="card-title">${title}</h5>
         <p class="card-text">${description}</p>
-        <small>Pages: ${numberOfPages}</small>
-        <small>${read ? 'Already read' : 'not read yet'}</small>
-        <button data-id='${id}' class='update-status' id='status-update'>Change read status</button>
-        <button data-id='${id}' class='remove-book'>Remove book</button>
+        <div>
+          <small>author: ${author}</small>
+        </div>
+        <div>
+          <small>Pages: ${numberOfPages}</small>, <small>${read ? 'Already read' : 'not read yet'}</small>
+        </div>
+        
+        <button data-id='${id}' class='btn btn-block btn-primary update-status' id='status-update'>Change read status</button>
+        <button data-id='${id}' class='btn btn-block btn-danger remove-book'>Remove book</button>
       </div>
     </div>
   </div>`;
@@ -44,30 +48,34 @@ Array.from(document.querySelectorAll('.remove-book')).map(el => {
       document.querySelector(`#book-${id}`).parentElement.removeChild(document.querySelector(`#book-${id}`));
     }
   });
+  return '';
 });
 
 Array.from(document.querySelectorAll('.update-status')).map(el => {
-  el.addEventListener('click', function (){ 
+  el.addEventListener('click', function () {
     const { id } = this.dataset;
-    const index = myLibrary.findIndex((ele) => ele.id == id);
+    const index = myLibrary.findIndex((ele) => ele.id === parseInt(id, 10));
     myLibrary[index].read = !myLibrary[index].read;
     window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
     window.location.assign('/');
   });
+  return '';
 });
 
 if (document.querySelector('#new-book')) {
   document.querySelector('#new-book').addEventListener('click', (e) => {
     e.preventDefault();
-    let title = document.querySelector('#title').value;
-    let image = document.querySelector('#image').value;
-    let description = document.querySelector('#description').value;
-    let author = document.querySelector('#author').value;
-    let numPages = document.querySelector('#numpages').value;
-    let read = document.querySelector('#read').checked;
-    let book = new Book(myLibrary[0].id+1, title, description, numPages, read, image);
+    const title = document.querySelector('#title').value;
+    const image = document.querySelector('#image').value;
+    const description = document.querySelector('#description').value;
+    const author = document.querySelector('#author').value;
+    const numPages = document.querySelector('#numpages').value;
+    const read = document.querySelector('#read').checked;
+    const book = new Book(myLibrary[0].id + 1, title, description, author, numPages, read, image);
     myLibrary = addBookToLibrary(book, myLibrary);
     window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
     window.location.assign('/');
   });
 }
+
+/* eslint-enable no-undef, func-names */
--- js/logic.js
+++ js/logic.js
@@ -1,19 +1,20 @@
-
-function Book(id, title, description, numberOfPages, read, image) {
+/* eslint-disable no-unused-vars, func-names */
+function Book(id, title, description, numberOfPages, author, read, image) {
   this.id = id;
   this.title = title;
   this.description = description;
+  this.author = author;
   this.numberOfPages = numberOfPages;
   this.read = read;
   this.image = image;
 }
 
-Book.prototype.updateStatus = function() { 
-	this.read = !this.read;
-}
+Book.prototype.updateStatus = function () {
+  this.read = !this.read;
+};
 
 function removeBook(id, library) {
-  const index = library.findIndex((ele) => ele.id == id);
+  const index = library.findIndex((ele) => ele.id === parseInt(id, 10));
   return [...library.slice(0, index), ...library.slice(index + 1)];
 }
 
@@ -21,4 +22,4 @@ function addBookToLibrary(book, library) {
   return [book, ...library];
 }
 
-export { Book, removeBook, addBookToLibrary };
+/* eslint-enable no-unused-vars, func-names */
